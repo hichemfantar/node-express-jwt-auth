fa87cc74bea8d59b0d4a5eb86241545acd4e7c53
Hichem Fantar <hichemfantar2049@gmail.com>
2020-12-27 13:25:33 +0100

    Cook away!

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+const { requireAuth, checkUser } = require("./middleware/authMiddleware");
+const { USERNAME, PASSWORD, DBNAME } = require("./config/env.json");
+
+const app = express();
+
+// middleware
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set("view engine", "ejs");
+
+// database connection
+const dbURI =
+  "mongodb+srv://" +
+  USERNAME +
+  ":" +
+  PASSWORD +
+  "@cluster0.vupwb.mongodb.net/" +
+  DBNAME;
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get("*", checkUser);
+app.get("/", (req, res) => res.render("home"));
+app.get("/recipes", requireAuth, (req, res) => res.render("recipes"));
+app.use(authRoutes);
